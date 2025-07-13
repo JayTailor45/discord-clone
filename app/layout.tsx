d5b3cf44fc001327395260a4ee173da13efc5041
JayTailor45 <tailorj64@gmail.com>
2025-07-13 18:23:35 +0530

    feat(dark-mode): add dark-light mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const openSans = Open_Sans({
   variable: "--font-geist-sans",
@@ -20,8 +22,24 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`${openSans.variable} antialiased`}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            openSans.className,
+            "bg-white",
+            "dark:bg-[#313338]",
+            "antialiased"
+          )}
+        >
+          <ThemeProvider
+            attribute={"class"}
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ components/providers/theme-provider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  ThemeProvider as NextThemeProvider,
+  ThemeProviderProps,
+} from "next-themes";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}
