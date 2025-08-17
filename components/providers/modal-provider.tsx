0e4c23c8e90f0ea4f58e2a4c6c63300ce392dcb4
JayTailor45 <tailorj64@gmail.com>
2025-08-17 17:13:48 +0530

    feat(server): add modal to update server name and image

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import CreateServerModal from "@/components/modals/create-server-modal";
-import { useModal } from "@/hooks/use-modal-store";
 import { useEffect, useState } from "react";
-import InviteModal from "../modals/invite-modal";
+
+import CreateServerModal from "@/components/modals/create-server-modal";
+import InviteModal from "@/components/modals/invite-modal";
+import EditServerModal from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
-  const { type } = useModal();
 
   useEffect(() => {
     setIsMounted(true);
@@ -21,6 +21,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
