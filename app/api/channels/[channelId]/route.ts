295b5e69e8fec9d823f6df7f52949a555b8c26d2
JayTailor45 <tailorj64@gmail.com>
2025-09-20 15:38:26 +0530

    feat(channel): add update channel support

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -51,7 +51,69 @@ export async function DELETE(
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log("CHANNEL_DELETE", error);
+    console.log("CHANNEL_ID_DELETE", error);
     return new NextResponse("Error deleting channel", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { channelId } = await params;
+    const { name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID is required", { status: 400 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID is required", { status: 400 });
+    }
+
+    if (name?.toLowerCase() === "general") {
+      return new NextResponse("Name cannot be 'general'", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: { in: [MemberRole.ADMIN, MemberRole.MODERATOR] },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: channelId,
+              name: {
+                not: "general",
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("CHANNEL_ID_PATCH", error);
+    return new NextResponse("Error updating channel", { status: 500 });
   }
 }
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  Channel,
-  ChannelType,
-  MemberRole,
-} from "@/lib/generated/prisma";
+import { Channel, ChannelType, MemberRole } from "@/lib/generated/prisma";
 import { cn } from "@/lib/utils";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { Edit, Hash, Mic, Trash, Video, Lock } from "lucide-react";
@@ -55,7 +51,10 @@ export const ServerChannel = ({
       {channel.name !== "general" && role !== MemberRole.GUEST && (
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
-            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Edit
+              onClick={() => onOpen("editChannel", { channel, server })}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
