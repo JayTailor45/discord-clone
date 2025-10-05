ba6df2a7cdb16bf5c064b117b61e3dc5ab61f166
JayTailor45 <tailorj64@gmail.com>
2025-10-05 12:51:07 +0530

    feat(chat): add ability to send messages

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { Member, Profile, Server } from "./lib/generated/prisma";
-import { Server as NetServer, Server } from "net";
+import { Server as NetServer } from "net";
 import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
 
